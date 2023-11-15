4f59ce3c6a78760e36caf175b238557e428a4338
Malcom Foca <malcom.foca@gmail.com>
2023-11-15 20:35:46 -0300

    ajustes finales de js

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+function buscar (){
+    alert('El producto que estas buscando no se encuentra en stock, realice una nueva busqueda')
+}
+
+function suscribirse (){
+    alert('Se suscribió correctamente a nuestro newsletter')
+}
